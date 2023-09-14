1162c46fe6bb462a8728c510c0af28ff8d31b55b
harmonymwirigi <harmonymwirigi99@gmail.com>
2023-09-14 23:42:58 +0300

    evronment variables

--- mozart/config.py
+++ mozart/config.py
@@ -2,8 +2,8 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    MAIL_SERVER = "smtp.googlemail.com"
-    MAIL_PORT = 587
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = os.environ.get('MAIL_PORT')
     MAIL_USE_TLS = "True"
-    MAIL_USERNAME = "kaberiaharmony@gmail.com"
-    MAIL_PASSWORD = "rbmfogqizwealjkv"
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
