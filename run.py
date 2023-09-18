b3dff5082ff141b1dfa1e19dbe0a315a452029e7
harmonymwirigi <harmonymwirigi99@gmail.com>
2023-09-18 15:23:54 +0300

    almost there

--- run.py
+++ run.py
@@ -1,6 +1,13 @@
-from mozart import create_app
+import os
+import sys
 
-app = create_app()
 
-if __name__ == '__main__':
-    app.run()
+sys.path.insert(0, os.path.dirname(__file__))
+
+
+def app(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    message = 'It works!\n'
+    version = 'Python v' + sys.version.split()[0] + '\n'
+    response = '\n'.join([message, version])
+    return [response.encode()]
